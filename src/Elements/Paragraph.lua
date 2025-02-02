91dafbac17edee8e852eb8333cb63b6d7d9b1794
kitsun6534 <idk256399@gmail.com>
2025-02-02 12:33:04 +0700

    .

--- src/Elements/Paragraph.lua
+++ src/Elements/Paragraph.lua
@@ -8,14 +8,22 @@ Paragraph.__index = Paragraph
 Paragraph.__type = "Paragraph"
 
 function Paragraph:New(Config)
-	assert(Config.Title, "Paragraph - Missing Title")
-	Config.Content = Config.Content or ""
+    assert(Config.Title, "Paragraph - Missing Title")
+    Config.Content = Config.Content or ""
 
-	local Paragraph = require(Components.Element)(Config.Title, Config.Content, Paragraph.Container, false)
-	Paragraph.Frame.BackgroundTransparency = 0.92
-	Paragraph.Border.Transparency = 0.6
+    local Paragraph = require(Components.Element)(Config.Title, Config.Content, Paragraph.Container, false)
+    Paragraph.Frame.BackgroundTransparency = 0.92
+    Paragraph.Border.Transparency = 0.6
 
-	return Paragraph
+    return Paragraph
+end
+
+function Paragraph:Set(Config)
+    self.Title = Config.Title or self.Title
+    self.Content = Config.Content or self.Content
+
+    self.Frame.Title.Text = self.Title
+    self.Frame.Content.Text = self.Content
 end
 
 return Paragraph
